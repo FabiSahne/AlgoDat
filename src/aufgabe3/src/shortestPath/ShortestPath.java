32ef6a63932260c5fa5839f049b4d6a8cc32e9bc
FabiSahne <hfranz90@gmail.com>
2024-06-11 14:10:01 +0200

    Finished Aufgabe 3

--- src/aufgabe3/src/shortestPath/ShortestPath.java
+++ src/aufgabe3/src/shortestPath/ShortestPath.java
@@ -6,76 +6,78 @@ package aufgabe3.src.shortestPath;
 import aufgabe2.graph.*;
 import aufgabe3.src.sim.SYSimulation;
 
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 // ...
 
 /**
  * Kürzeste Wege in Graphen mit A*- und Dijkstra-Verfahren.
+ *
+ * @param <V> Knotentyp.
  * @author Oliver Bittel
  * @since 27.01.2015
- * @param <V> Knotentyp.
  */
 public class ShortestPath<V> {
-	
-	SYSimulation sim = null;
-	
-	Map<V,Double> dist; 		// Distanz für jeden Knoten
-	Map<V,V> pred; 				// Vorgänger für jeden Knoten
-	IndexMinPQ<V,Double> cand; 	// Kandidaten als PriorityQueue PQ
-	DirectedGraph<V> graph; 	// Gerichteter Graph
+
+    SYSimulation sim = null;
+
+    Map<V, Double> dist;        // Distanz für jeden Knoten
+    Map<V, V> pred;                // Vorgänger für jeden Knoten
+    IndexMinPQ<V, Double> cand;    // Kandidaten als PriorityQueue PQ
+    DirectedGraph<V> graph;    // Gerichteter Graph
     Heuristic<V> heur;             // Heuristik
     List<V> path;
     double distance;
 
-	/**
-	 * Konstruiert ein Objekt, das im Graph g k&uuml;rzeste Wege 
-	 * nach dem A*-Verfahren berechnen kann.
-	 * Die Heuristik h schätzt die Kosten zwischen zwei Knoten ab.
-	 * Wird h = null gewählt, dann ist das Verfahren identisch 
-	 * mit dem Dijkstra-Verfahren.
-	 * @param g Gerichteter Graph
-	 * @param h Heuristik. Falls h == null, werden kürzeste Wege nach
-	 * dem Dijkstra-Verfahren gesucht.
-	 */
-	public ShortestPath(DirectedGraph<V> g, Heuristic<V> h) {
-        path = new LinkedList<>();
-		dist = new HashMap<>();
-		pred = new HashMap<>();
-		cand = new IndexMinPQ<>();
+    /**
+     * Konstruiert ein Objekt, das im Graph g k&uuml;rzeste Wege
+     * nach dem A*-Verfahren berechnen kann.
+     * Die Heuristik h schätzt die Kosten zwischen zwei Knoten ab.
+     * Wird h = null gewählt, dann ist das Verfahren identisch
+     * mit dem Dijkstra-Verfahren.
+     *
+     * @param g Gerichteter Graph
+     * @param h Heuristik. Falls h == null, werden kürzeste Wege nach
+     *          dem Dijkstra-Verfahren gesucht.
+     */
+    public ShortestPath(DirectedGraph<V> g, Heuristic<V> h) {
+        path = new ArrayList<>();
+        dist = new HashMap<>();
+        pred = new HashMap<>();
+        cand = new IndexMinPQ<>();
         heur = h;
         graph = g;
         distance = 0.0;
-	}
+    }
 
-	/**
-	 * Diese Methode sollte nur verwendet werden, 
-	 * wenn kürzeste Wege in Scotland-Yard-Plan gesucht werden.
-	 * Es ist dann ein Objekt für die Scotland-Yard-Simulation zu übergeben.
-	 * <p>
-	 * Ein typische Aufruf für ein SYSimulation-Objekt sim sieht wie folgt aus:
-	 * <p><blockquote><pre>
-	 *    if (sim != null)
-	 *       sim.visitStation((Integer) v, Color.blue);
-	 * </pre></blockquote>
-	 * @param sim SYSimulation-Objekt.
-	 */
-	public void setSimulator(SYSimulation sim) {
-		this.sim = sim;
-	}
+    /**
+     * Diese Methode sollte nur verwendet werden,
+     * wenn kürzeste Wege in Scotland-Yard-Plan gesucht werden.
+     * Es ist dann ein Objekt für die Scotland-Yard-Simulation zu übergeben.
+     * <p>
+     * Ein typische Aufruf für ein SYSimulation-Objekt sim sieht wie folgt aus:
+     * <p><blockquote><pre>
+     *    if (sim != null)
+     *       sim.visitStation((Integer) v, Color.blue);
+     * </pre></blockquote>
+     *
+     * @param sim SYSimulation-Objekt.
+     */
+    public void setSimulator(SYSimulation sim) {
+        this.sim = sim;
+    }
 
-	/**
-	 * Sucht den kürzesten Weg von Starknoten s zum Zielknoten g.
-	 * <p>
-	 * Falls die Simulation mit setSimulator(sim) aktiviert wurde, wird der Knoten,
-	 * der als nächstes aus der Kandidatenliste besucht wird, animiert.
-	 * @param s Startknoten
-	 * @param g Zielknoten
-	 */
-	public void searchShortestPath(V s, V g) {
+    /**
+     * Sucht den kürzesten Weg von Starknoten s zum Zielknoten g.
+     * <p>
+     * Falls die Simulation mit setSimulator(sim) aktiviert wurde, wird der Knoten,
+     * der als nächstes aus der Kandidatenliste besucht wird, animiert.
+     *
+     * @param s Startknoten
+     * @param g Zielknoten
+     */
+    public void searchShortestPath(V s, V g) {
+        path.clear();
         cand.clear();
         dist.clear();
         pred.clear();
@@ -87,18 +89,16 @@ public class ShortestPath<V> {
         dist.put(s, 0.0);
 
         if (heur == null) {
-            searchShortestPathDijkstra(s);
+            searchShortestPathDijkstra(s, g);
         } else {
             searchShortestPathAStar(s, g);
         }
 
+        distance = dist.get(g);
         V v = g;
         while (v != null) {
             path.addFirst(v);
-            V w = pred.get(v);
-            if (w != null)
-                distance += dist.get(v) - dist.get(w);
-            v = w;
+            v = pred.get(v);
         }
 
         if (sim != null) {
@@ -106,7 +106,7 @@ public class ShortestPath<V> {
                 sim.drive((Integer) path.get(i), (Integer) path.get(i + 1), java.awt.Color.red);
             }
         }
-	}
+    }
 
 
     private boolean searchShortestPathAStar(V s, V g) {
@@ -115,12 +115,12 @@ public class ShortestPath<V> {
 
         while (!cand.isEmpty()) {
             V v = cand.removeMin();
-            //System.out.println("Besuche Knoten " + v + " mit d = " + dist.get(v));
+            System.out.println("Besuche Knoten " + v + " mit d = " + dist.get(v));
+            if (sim != null)
+                sim.visitStation((Integer) v, java.awt.Color.blue);
             if (v.equals(g)) {
                 return true;
             }
-            if (sim != null)
-                sim.visitStation((Integer) v, java.awt.Color.blue);
             for (V w : graph.getSuccessorVertexSet(v)) {
                 if (dist.get(w) == Double.POSITIVE_INFINITY) {
                     pred.put(w, v);
@@ -136,19 +136,23 @@ public class ShortestPath<V> {
         return false;
     }
 
-    private void searchShortestPathDijkstra(V s) {
+    private void searchShortestPathDijkstra(V s, V g) {
         cand.add(s, 0.0);
 
         while (!cand.isEmpty()) {
             V v = cand.removeMin();
+            System.out.println("Besuche Knoten " + v + " mit d = " + dist.get(v));
             if (sim != null)
                 sim.visitStation((Integer) v, java.awt.Color.blue);
+            if (v.equals(g)) {
+                return;
+            }
             for (V w : graph.getSuccessorVertexSet(v)) {
                 if (dist.get(w) == Double.POSITIVE_INFINITY) {
                     pred.put(w, v);
                     dist.put(w, dist.get(v) + graph.getWeight(v, w));
                     cand.add(w, dist.get(w));
-                } else if ( dist.get(v) + graph.getWeight(v, w) < dist.get(w) ) {
+                } else if (dist.get(v) + graph.getWeight(v, w) < dist.get(w)) {
                     pred.put(w, v);
                     dist.put(w, dist.get(v) + graph.getWeight(v, w));
                     cand.change(w, dist.get(w));
@@ -158,29 +162,31 @@ public class ShortestPath<V> {
     }
 
     /**
-	 * Liefert einen kürzesten Weg von Startknoten s nach Zielknoten g.
-	 * Setzt eine erfolgreiche Suche von searchShortestPath(s,g) voraus.
-	 * @throws IllegalArgumentException falls kein kürzester Weg berechnet wurde.
-	 * @return kürzester Weg als Liste von Knoten.
-	 */
-	public List<V> getShortestPath() {
-		if (pred.isEmpty()) {
+     * Liefert einen kürzesten Weg von Startknoten s nach Zielknoten g.
+     * Setzt eine erfolgreiche Suche von searchShortestPath(s,g) voraus.
+     *
+     * @return kürzester Weg als Liste von Knoten.
+     * @throws IllegalArgumentException falls kein kürzester Weg berechnet wurde.
+     */
+    public List<V> getShortestPath() {
+        if (pred.isEmpty()) {
             throw new IllegalArgumentException("Kein kürzester Weg berechnet.");
         }
         return path;
-	}
+    }
 
-	/**
-	 * Liefert die Länge eines kürzesten Weges von Startknoten s nach Zielknoten g zurück.
-	 * Setzt eine erfolgreiche Suche von searchShortestPath(s,g) voraus.
-	 * @throws IllegalArgumentException falls kein kürzester Weg berechnet wurde.
-	 * @return Länge eines kürzesten Weges.
-	 */
-	public double getDistance() {
+    /**
+     * Liefert die Länge eines kürzesten Weges von Startknoten s nach Zielknoten g zurück.
+     * Setzt eine erfolgreiche Suche von searchShortestPath(s,g) voraus.
+     *
+     * @return Länge eines kürzesten Weges.
+     * @throws IllegalArgumentException falls kein kürzester Weg berechnet wurde.
+     */
+    public double getDistance() {
         if (dist.isEmpty()) {
             throw new IllegalArgumentException("Kein kürzester Weg berechnet.");
         }
         return distance;
-	}
+    }
 
 }
